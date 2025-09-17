2787ca3c7fbb5d9c5ea6f2c383e6858deb12ab9e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-17 14:27:46 +0530

    Refactor build script: consolidate test configuration and dependency setup in kotlin-playground module

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -8,6 +8,14 @@ plugins {
 group = libs.versions.kotlinPlayground.group.get()
 version = libs.versions.kotlinPlayground.version.get()
 
+application {
+    mainClass = libs.versions.kotlinPlayground.mainClass.get()
+}
+
+dependencies {
+    testImplementation(libs.kotlin.test)
+}
+
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of("23")
@@ -22,12 +30,8 @@ kotlin {
     }
 }
 
-application {
-    mainClass = libs.versions.kotlinPlayground.mainClass.get()
-}
-
-dependencies {
-    testImplementation(libs.kotlin.test)
+tasks.test {
+    useJUnitPlatform()
 }
 
 // Custom build directory
