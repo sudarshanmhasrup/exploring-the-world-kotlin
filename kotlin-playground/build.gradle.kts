cb26535db093a9f53ef831be3b0c9e7b70ba8bfe
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-17 13:38:53 +0530

    Set Java toolchain to version 23 and configure Kotlin JVM target to JVM 23 in the kotlin-playground module

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,9 +1,25 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
 group = libs.versions.kotlinPlayground.group.get()
 version = libs.versions.kotlinPlayground.version.get()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("23")
+    }
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_23)
+    }
+}
 
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/KotlinPlayground"))
